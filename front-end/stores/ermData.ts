4e3381333293485bbe548ffb67dbedd0cee3572a
qianqian <qianqian@mandarinhsk.com>
2025-05-26 01:07:38 +0800

    保留两位小数

--- front-end/stores/ermData.ts
+++ front-end/stores/ermData.ts
@@ -277,9 +277,9 @@ class ErmData {
                 averagePrice: number
               }
               if (price) {
-                material.highestPrice = price.highestPrice
-                material.lowestPrice = price.lowestPrice
-                material.averagePrice = price.averagePrice
+                material.highestPrice = parseInt(price.highestPrice.toFixed(2))
+                material.lowestPrice = parseInt(price.lowestPrice.toFixed(2))
+                material.averagePrice = parseInt(price.averagePrice.toFixed(2))
               }
             })
           })
